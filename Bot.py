1967ed2592f4f60d04fa35d954335fd781c7997e
timedaily03-lang <timedaily03@gmail.com>
2026-02-23 20:00:19 +0530

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -5,8 +5,7 @@ from yt_dlp import YoutubeDL
 
 # Unga Details
 API_ID = 33067910
-API_HASH = 
-"9d8f59413d03f6a9239d4af8279641c2"
+API_HASH = "9d8f59413d03f6a9239d4af8279641c2"
 BOT_TOKEN =
 "88773807659:AAEtjHhdXKkpb3cGLCnfh3Ifs5qVxUqx6_w"
 
